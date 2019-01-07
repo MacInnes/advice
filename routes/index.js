fd5823c01f45a9306f2c24b589dcc241aaf40305
MacInnes <califormula@gmail.com>
2019-01-07 13:45:34 -0700

    basic search functionality working

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Advice' });
+  res.render('index');
 });
 
-router.post('/search', function(req, res, next) {
+router.post('/search', async function(req, res, next) {
   console.log(req.body.subject);
-  
+  var adviceResponse = new Promise((resolve, reject) => {
+    var options = {
+      url: `https://api.adviceslip.com/advice/search/${req.body.subject}`
+    }
+    request(options, function(err, response, body){
+      var responseJSON = JSON.parse(response.body);
+      var rand = responseJSON.slips[Math.floor(Math.random() * responseJSON.slips.length)];
+      resolve(rand);
+    })
+  })
+  var advice = await adviceResponse;
+  console.log(advice);
+  res.render('index', { advice: advice })
 })
 
 module.exports = router;
