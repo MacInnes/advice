24fe50214df476d1def9e24a5f5f545a0b240c06
MacInnes <califormula@gmail.com>
2019-01-07 10:58:34 -0700

    adds coverage to gitignore

--- test/features.spec.js
+++ test/features.spec.js
@@ -6,3 +6,15 @@ const server = require('../app.js');
 const environment = process.env.NODE_ENV || 'development';
 
 chai.use(chaiHttp);
+
+describe('root route', () => {
+  it('can get root', done => {
+    chai.request(server)
+      .get("/")
+      .end((err, response) => {
+
+        response.should.have.status(200);
+        done();
+      });
+  });
+})
