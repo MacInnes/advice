0bd6bc3ee08e4695408243aa1b86099834407be2
MacInnes <califormula@gmail.com>
2019-01-07 10:22:07 -0700

    adds test file

---
+++ test/features.spec.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../app.js');
+
+const environment = process.env.NODE_ENV || 'development';
+
+chai.use(chaiHttp);
