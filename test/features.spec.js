430a36557ccea8f1b47aa3e0f562b8079b13799f
MacInnes <califormula@gmail.com>
2019-01-07 11:26:58 -0700

    initial testing setup

--- test/features.spec.js
+++ test/features.spec.js
@@ -1,20 +1,23 @@
+const sw = require('selenium-webdriver');
+
+const driver = new sw.Builder()
+    .forBrowser('safari')
+    .build();
+
 const chai = require('chai');
+const chaiWebdriver = require('chai-webdriver');
+chai.use(chaiWebdriver(driver));
+
 const should = chai.should();
-const chaiHttp = require('chai-http');
+
 const server = require('../app.js');
 
 const environment = process.env.NODE_ENV || 'development';
 
-chai.use(chaiHttp);
-
 describe('root route', () => {
   it('can get root', done => {
-    chai.request(server)
-      .get("/")
-      .end((err, response) => {
-
-        response.should.have.status(200);
-        done();
-      });
+    driver.get('http://localhost:3000');
+    chai.expect('body').dom.to.contain.text("Welcome to Express");
+    done();
   });
 })
